993ec44c74d2ba3baa2532095c74097d7f3566da
chelseazqc <39674376+chelseazqc@users.noreply.github.com>
2020-05-16 21:55:51 +0800

    增加了僵尸类

--- constants.py
+++ constants.py
@@ -3,3 +3,32 @@ SunFlower_PATH="resources/pics/plants/SunFlower_0.png"
 PeaShooter_PATH="resources/pics/plants/Peashooter_0.png"
 BulletNormal_PATH="resources/pics/bullets/PeaNormal_0.png"
 BulletExplode_PATH="resources/pics/bullets/PeaNormalExplode_0.png"
+
+PRODUCE_SUN_INTERVAL = 7000
+FLOWER_SUN_INTERVAL = 22000
+SUN_LIVE_TIME = 7000
+SUN_VALUE = 25
+
+
+LOSTHEAD_HEALTH = 5
+NORMAL_HEALTH = 10
+
+ATTACK_INTERVAL = 1000
+ZOMBIE_WALK_INTERVAL = 70
+FREEZE_INTERVAL=100
+
+ZOMBIE_START_X = SCREEN_WIDTH + 50
+
+#STATE
+ATTACK = 'attack'
+ATTACKED = 'attacked'
+WALK = 'walk'
+DIE = 'die'
+FREEZE = 'freeze'
+SLEEP = 'sleep'
+
+
+#BACKGROUND
+BACKGROUND_DAY = 0
+BACKGROUND_NIGHT = 1
+
