32513bc19a18eba36c29635f1edb4501a6c8523c
maxzheng <1836941753@qq.com>
2020-05-26 12:37:42 +0800

    简单些了下Car和Sun 待完善

--- Model/Car.py
+++ Model/Car.py
@@ -1,11 +1,37 @@
+from enum import Enum
+
 import pygame
 
 from Model.MySprite import MySprite
 from constants import PIC_CAR_PATH
 
 
-class Car(MySprite):
-    def __init__(self):
-        carimg = pygame.image.load("../" + PIC_CAR_PATH).convert_alpha()
-        super().__init__(carimg)
+class CarState(Enum):
+    STATIC = 1  # 静止状态
+    MOVING = 2
 
+
+class Car(MySprite):
+
+    def __init__(self, posx, posy):
+        carimg = pygame.image.load("../" + PIC_CAR_PATH).convert_alpha()
+
+        super().__init__(carimg)
+        self.position = posx, posy
+
+        self._state = CarState.STATIC
+
+        self.speed = 1
+
+    def move(self):
+        self._state = CarState.MOVING
+
+    def isMoving(self):
+        return self._state == CarState.MOVING
+
+    def update(self):
+        # super.update(self)
+        if self._state == CarState.STATIC:
+            pass
+        elif self._state == CarState.MOVING:
+            self.X += self.speed
